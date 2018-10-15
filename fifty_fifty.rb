b591bf8d2667b99ea831c28b86ba17599f3faa57
Kris <krisholley_09@hotmail.com>
2018-10-15 13:47:11 -0500

    finally working

--- fifty_fifty.rb
+++ fifty_fifty.rb
@@ -8,3 +8,59 @@ def winner(my_num, bash_num)
     end
     return arr1
 end
+
+def offbyonenumber(my_number, bash_numbers)
+    counter = 0
+    x = 0
+    arr1 = []
+    my_number.each do |num|
+        bash_numbers.each do |bash|
+            num.length.times do
+                if bash[x] == num[x]
+                        counter += 1
+                    if counter == num.length - 1
+                        arr1<< num
+                        counter = 0
+                    end 
+                end
+                x += 1
+            end
+        end
+    end
+     arr1.uniq!
+    p arr1
+    return arr1
+end
+
+# def off_by_one_number(my_num, bash_num)
+#     counter = 0
+#     x = 0
+#     arr1 =[]
+#     my_num.each do |num|
+#        bash_num.each do |bash|
+#             num.length.times do
+#                 p "this is x #{x}"
+#                 if bash[x] == num[x]
+#                          counter += 1  
+#                          puts "bash x #{bash[x]}"
+#                          puts "num x #{num[x]}"
+#                       p "the counter is #{counter}"
+#                      if counter == num.length - 1 
+#                         # p "add to array #{counter} = #{my_num.length - 1}"
+#                          arr1<< num
+#                         #  counter = 0
+#                         #  x = 0
+#                     end
+#                     x += 1
+#                end
+#             #    counter = 0
+#             #    x = 0
+#             end 
+#             counter = 0
+#        end
+
+#     end
+#     arr1.uniq!
+#      p arr1
+#      return arr1
+# end
--- test_fifty_fifty.rb
+++ test_fifty_fifty.rb
@@ -17,5 +17,33 @@ class My_test < Minitest::Test
         bash_num = ["1234", "6123", "1586", "9999"]
         assert_equal(["6123", "9999"], winner(my_num, bash_num))
     end
+
+    def test_num_4
+        my_number = ["1555", "6123", "9999"]
+        bash_numbers = ["1234", "6122", "1585", "5999"]
+        assert_equal(["1555", "6123", "9999"], offbyonenumber(my_number, bash_numbers))
+    end
+
+    def test_num_5
+        my_number = ["1235"]
+        bash_numbers = ["1230", "8888", "6654"]
+        assert_equal(["1235"], offbyonenumber(my_number, bash_numbers))
+    end
+
+    # def test_num_6
+    #     my_number = ["1234", "9876", "5432", "7777", "3333"]
+    #     bash_numbers = ["8675", "3099", "6777", "2333", "3825", "1478", "8523", "9632"]
+    #     assert_equal(["7777", "3333"], offbyonenumber(my_number, bash_numbers))
+    # end
+    # def test_num_7
+    #     my_number = ["1234"]
+    #     bash_numbers = ["1233"]
+    #     assert_equal(["1234"], offbyonenumber(my_number, bash_numbers))
+    # end
+
+    def test_six
+    my_number = ["1234", "2234", "3234"]
+    bash_numbers = ["1233", "2233", "3233", "2222", "5555"]
+    assert_equal(["1234", "2234", "3234"], offbyonenumber(my_number, bash_numbers))
+  end
 end 
-    
